69c86d3697c506dca62295c5183ff05da7e4ab75
sharon <harold.campbell@gmail.com>
2015-01-23 08:56:45 +0200

    chore 0.0.5-5
    Fully automated build with js replacement

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,9 +2,17 @@ module.exports = function (grunt) {
 
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
+        uglified_name: 'js/<%= pkg.name %>.min.js',
         css_assets: [
             "<link rel=\"stylesheet\" href=\"//maxcdn.bootstrapcdn.com/bootstrap/3.3.2/css/bootstrap.min.css\">",
             "<link rel=\"stylesheet\" href=\"css/<%= pkg.name %>.css\">"
+        ],
+
+        js_assets: [
+            "<script src=\"https://code.jquery.com/jquery-2.1.3.min.js\"></script>",
+            "<script src=\"https://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular.min.js\"></script>",
+            "<script src=\"https://ajax.googleapis.com/ajax/libs/angularjs/1.3.8/angular-route.min.js\"></script>",
+            "<script src=\"<%= uglified_name %>\"></script>"
         ],
 
         clean: {
@@ -14,9 +22,6 @@ module.exports = function (grunt) {
         },
 
         concat: {
-//            options: {
-//                separator: ';'
-//            },
             js: {
                 options: {
                     separator: ';'
@@ -48,7 +53,7 @@ module.exports = function (grunt) {
             },
             dist: {
                 files: {
-                    'build/js/<%= pkg.name %>.min.js': ['<%= concat.js.dest %>']
+                    'build/<%= uglified_name %>': ['<%= concat.js.dest %>']
                 }
             }
         },
@@ -68,7 +73,6 @@ module.exports = function (grunt) {
 
         watch: {
             files: ['<%= jshint.files %>'],
-//            tasks: ['jshint', 'qunit']
             tasks: ['jshint']
         },
 
@@ -87,30 +91,48 @@ module.exports = function (grunt) {
             version: {
                 src: ['index.html'],
                 overwrite: true,                 // overwrite matched source files
-                replacements: [{
-                    from: /<div class="version">[0-9]\.[0-9]\.[0-9](-[0-9])?<\/div>/g,
-                    to: "<div class=\"version\"><%= pkg.version %></div>"
-                }]
+                replacements: [
+                    {
+                        from: /<div class="version">[0-9]\.[0-9]\.[0-9](-[0-9])?<\/div>/g,
+                        to: "<div class=\"version\"><%= pkg.version %></div>"
+                    }
+                ]
             },
+            /** Replaces the css files in the index.html file with the merged one and the bootstrap cdn */
             styles: {
                 src: ['build/index.html'],
                 overwrite: true,                 // overwrite matched source files
-                replacements: [{
-                    from: /<!--\[\[css-->[\S\s<>\.="]*<!--css]]-->/g,
-                    to: "<%= css_assets.join(\"\\n\") %>"
-                }]
+                replacements: [
+                    {
+                        from: /<!--\[\[css-->[\S\s<>\.="]*<!--css]]-->/g,
+                        to: "<%= css_assets.join(\"\\n\\t\") %>"
+                    }
+                ]
+            },
+            /** Replaces the js files in the index.html file with the min'd files and the different js cdns */
+            js: {
+                src: ['build/index.html'],
+                overwrite: true,                 // overwrite matched source files
+                replacements: [
+                    {
+                        from: /<!--\[\[js-->[\S\s<>\.="]*<!--js]]-->/g,
+                        to: "<%= js_assets.join(\"\\n\\t\") %>"
+                    }
+                ]
             }
         },
 
         sass: {
             dist: {
-                files: [{
-                    expand: true,
-                    cwd: 'assets',
-                    src: ['sass/**/*.scss'],
-                    dest: 'assets/css/',
-                    ext: '.css'
-                }]
+                files: [
+                    {
+                        expand: true,
+                        cwd: 'assets',
+                        src: ['sass/**/*.scss'],
+                        dest: 'assets/css/',
+                        ext: '.css'
+                    }
+                ]
             }
         }
     });
@@ -128,7 +150,9 @@ module.exports = function (grunt) {
     grunt.registerTask('default', ['jshint']);
     grunt.registerTask('bump-ver', "Bump the version in the package.json and index.html files.", ['bump', 'replace:version']);
     grunt.registerTask('test', ['jshint']);
-    grunt.registerTask('build', ['sass', 'clean', 'concat', 'uglify', 'replace:version', 'copy:nonjssite']);
+    grunt.registerTask('replace_assets', ['replace:styles', 'replace:js']);
+
+    grunt.registerTask('build', ['sass', 'clean', 'concat', 'uglify', 'replace:version', 'copy:nonjssite', 'replace_assets']);
 //    grunt.registerTask('default', ['jshint', 'qunit', 'concat', 'uglify']);
 
 };
